d8b4e2390539125b15a67b0505e62d87b2047a34
SilvestrePedro <48075175+SilvestrePedro@users.noreply.github.com>
2019-03-21 21:45:32 -0300

    Add files via upload

---
+++ WHILE.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+    int main(){
+        int contador = -1;
+
+        while (contador <= 10000){
+     printf("%d ", contador);
+
+     contador ++;
+    }
+
+    return 0;
+    }
